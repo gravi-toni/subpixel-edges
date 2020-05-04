11a2123e97bfe46069ccfb050f2ef4a6e0da5d48
nvitucci <nicola.vitucci@gmail.com>
2020-05-04 08:49:13 +0100

    Added init method

--- examples/example.py
+++ examples/example.py
@@ -5,21 +5,29 @@ import matplotlib.pyplot as plt
 
 from time import time
 
-from subpixel_edges import subpixel_edges
+from subpixel_edges import init, subpixel_edges
 
 
 if __name__ == '__main__':
     this_path = os.path.dirname(os.path.realpath(__file__))
     img = cv2.imread(os.path.join(this_path, 'images/saturn.jpg'))
     img_gray = (cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)).astype(float)
+
     iters = 1
     threshold = 15
     order = 2
 
+    print("Initializing...")
+    now = time()
+    init()
+    elapsed = time() - now
+    print("Initialization time: ", elapsed)
+
+    print("Processing...")
     now = time()
     edges = subpixel_edges(img_gray, threshold, iters, order)
     elapsed = time() - now
-    print(elapsed)
+    print("Processing time: ", elapsed)
 
     plt.imshow(img[..., ::-1])
     # seg = 0.6
--- subpixel_edges/__init__.py
+++ subpixel_edges/__init__.py
@@ -1,6 +1,16 @@
+import numpy as np
+
 from subpixel_edges.final_detector_iter0 import main_iter0
 from subpixel_edges.final_detector_iter1 import main_iter1
 from subpixel_edges.final_detector_iterN import main_iterN
+
+
+def init():
+    img = np.zeros((8, 8))
+
+    subpixel_edges(img, 15, 0, 2)
+    subpixel_edges(img, 15, 1, 2)
+    subpixel_edges(img, 15, 2, 2)
 
 
 def subpixel_edges(img, threshold, iters, order):
