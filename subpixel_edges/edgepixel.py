e96b3b603195effad245a5d901e0b7274326b5a8
nvitucci <nicola.vitucci@gmail.com>
2020-05-26 23:22:13 +0100

    Added some doc

--- subpixel_edges/edgepixel.py
+++ subpixel_edges/edgepixel.py
@@ -1,4 +1,18 @@
 class EdgePixel:
+    """
+    Class describing pixel features.
+
+    position:
+        1D-index inside image
+    x, y:
+        subpixel position
+    nx, ny:
+        normal vector (normalized)
+    curv:
+        curvature
+    i0, i1:
+        intensities at both sides
+    """
     def __init__(self, position=None, x=None, y=None, nx=None, ny=None, curv=None, i0=None, i1=None):
         
         self.position = position  # 1D index inside image
