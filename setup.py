e3f07eb5e803e502b0df3865c3cecd383840223c
nvitucci <nicola.vitucci@gmail.com>
2020-06-08 21:04:21 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as readme:
 
 setuptools.setup(
     name='subpixel-edges',
-    version='0.0.3',
+    version='0.1.0',
     author='GraviToni',
     description='Pure Python implementation of subpixel edge location algorithm based on partial area effect',
     long_description=long_description,
